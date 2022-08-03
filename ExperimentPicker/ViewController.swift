f9cf57af86baaad68a58078f672bb013b3ed433f
priferr <priferr@gmail.com>
2022-08-03 17:01:29 -0300

    Test for Udacity Course

--- ExperimentPicker/ViewController.swift
+++ ExperimentPicker/ViewController.swift
@@ -11,8 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
+    
+    @IBAction func experiment(_ sender: UIButton) {
+    let nextController = UIImagePickerController()
+        self.present(nextController, animated: true, completion: nil)
+    }
+    
+    @IBAction func experimentActivity(_ sender: Any) {
+
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func experimentAlert(_ sender: Any) {
+
+        let controller = UIAlertController()
+            controller.title = "Test Alert"
+            controller.message = "This is a test"
+
+        let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default) { action in self.dismiss(animated: true, completion: nil)
+            }
+
+            controller.addAction(okAction)
+            self.present(controller, animated: true, completion: nil)
+        }
 
 
 }
